63e237c79e3dfb42a0cdaf4ab980bed25c05ed0e
Stanislaw Cruz <stanycruz@outlook.com>
2019-05-25 22:47:19 -0300

    First commit.

---
+++ Pessoa.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace iequatable
+{
+    internal class Pessoa : IEquatable<Pessoa>
+    {
+        private string nome;
+        private string cpf;
+        private string rg;
+        private DateTime dataNascimento;
+
+        public Pessoa(string nome, string cpf, string rg, DateTime dataNascimento)
+        {
+            this.nome = nome;
+            this.cpf = cpf;
+            this.rg = rg;
+            this.dataNascimento = dataNascimento;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Cpf
+        {
+            get { return cpf; }
+            set { cpf = value; }
+        }
+
+        public string Rg
+        {
+            get { return rg; }
+            set { rg = value; }
+        }
+
+        public DateTime DataNascimento
+        {
+            get { return dataNascimento; }
+            set { dataNascimento = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"[Nome: {this.nome}, CPF: {this.cpf}, RG: {this.rg}, Data Nascimento: {this.dataNascimento}]";
+        }
+
+        public bool Equals(Pessoa outro)
+        {
+            if (outro == null) return false;
+
+            return
+                this.nome.Equals(outro.nome) &&
+                this.cpf.Equals(outro.cpf) &&
+                this.rg.Equals(outro.rg) &&
+                this.dataNascimento.Equals(outro.dataNascimento);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var outro = obj as Pessoa;
+
+            if (outro == null) return false;
+
+            return this.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return
+                    this.nome.GetHashCode() ^
+                    this.cpf.GetHashCode() ^
+                    this.rg.GetHashCode() ^
+                    this.dataNascimento.GetHashCode();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace iequatable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Pessoa> pessoas1 = new Pessoa[]
+            {
+                new Pessoa("Renan Osvaldo Benício Dias", "218.523.393-90", "19.030.419-4", Convert.ToDateTime("01/08/1952")),
+                new Pessoa("Patrícia Fátima Costa", "328.436.702-09", "31.519.736-5", Convert.ToDateTime("06/06/1980")),
+                new Pessoa("Eduardo Miguel Ferreira", "879.996.099-00", "20.114.904-7", Convert.ToDateTime("21/06/1946")),
+                new Pessoa("Arthur José Bernardo Aragão", "170.726.840-18", "35.549.099-7", Convert.ToDateTime("11/02/1993")),
+                new Pessoa("Giovanni Murilo Gael da Cruz", "956.193.702-67", "13.095.098-1", Convert.ToDateTime("04/02/1994")),
+                //mais seis pessoas em 1 que não contém em 2
+                new Pessoa("Diego Antonio Vicente Lima", "290.708.148-97", "31.490.639-3", Convert.ToDateTime("19/03/1992")),
+                new Pessoa("Levi Yuri Fernandes", "217.024.637-10", "12.636.006-6", Convert.ToDateTime("15/03/1965")),
+                new Pessoa("Sérgio Enzo Mário Aragão", "220.435.884-35", "19.895.201-6", Convert.ToDateTime("14/07/1958")),
+                new Pessoa("Carlos Raimundo Nicolas Mendes", "080.994.941-56", "080.994.941-56", Convert.ToDateTime("08/03/1972")),
+                new Pessoa("Breno Gabriel Jorge Barros", "074.080.994-68", "48.030.880-9", Convert.ToDateTime("21/08/1977")),
+                new Pessoa("Emily Alícia Lima", "798.079.359-54", "38.411.770-3", Convert.ToDateTime("02/01/1972"))
+            };
+
+            IEnumerable<Pessoa> pessoas2 = new Pessoa[]
+            {
+                new Pessoa("Renan Osvaldo Benício Dias", "218.523.393-90", "19.030.419-4", Convert.ToDateTime("01/08/1952")),
+                new Pessoa("Patrícia Fátima Costa", "328.436.702-09", "31.519.736-5", Convert.ToDateTime("06/06/1980")),
+                new Pessoa("Eduardo Miguel Ferreira", "879.996.099-00", "20.114.904-7", Convert.ToDateTime("21/06/1946")),
+                new Pessoa("Arthur José Bernardo Aragão", "170.726.840-18", "35.549.099-7", Convert.ToDateTime("11/02/1993")),
+                new Pessoa("Giovanni Murilo Gael da Cruz", "956.193.702-67", "13.095.098-1", Convert.ToDateTime("04/02/1994"))
+            };
+
+            ISet<Pessoa> set1 = new HashSet<Pessoa>(pessoas1);
+            ISet<Pessoa> set2 = new HashSet<Pessoa>(pessoas2);
+
+            var compare = set1.Except(set2);
+
+            Console.WriteLine($"-- {compare.Count()} Resultados --");
+
+            foreach (var item in compare)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
